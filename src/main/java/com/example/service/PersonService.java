f37cd5a979fdca3a3ffbfd4ee06de2dc6f3f6261
Laalhussein <128274860+Laalhussein@users.noreply.github.com>
2023-03-26 04:37:15 +0300

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -42,7 +42,7 @@ public class PersonService {
 		
 		p = new Person();
 		p.setId("5");
-		p.setAge(24);
+		p.setAge(27);
 		p.setFirstName("Lamya");
 		p.setLastName("Abdullah");
 		persons.add(p);
